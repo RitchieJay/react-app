5fa2014c6720460ef0f9b043945d0b440f1b7c1e
RitchieJaye <richardjohnson@halesowen.ac.uk>
2023-08-16 21:47:37 +0100

    Consolidated final version of TODO List.

---
+++ src/TodoItem.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+  return (
+    <li>
+      <label>
+        <input
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+          type='checkbox'
+          checked={completed}
+        />
+        {title}
+      </label>
+      <button
+        onClick={() => deleteTodo(id)}
+        className='btn btn-danger'>
+        Delete
+      </button>
+    </li>
+  );
+}
